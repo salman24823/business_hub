96fd5461d14ba2ce985f2578e0c819e937d3dfe6
Ahmad <95245638+Ahmaddar007@users.noreply.github.com>
2025-02-28 12:18:30 +0500

    ....

--- src/app/Sections/VisionSection/page.jsx
+++ src/app/Sections/VisionSection/page.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import v1 from '../../../../public/v1.jpg';
+import v2 from '../../../../public/v2.jpg';
+import v3 from '../../../../public/v3.jpg';
+import v4 from '../../../../public/v4.jpg';
+import Image from 'next/image';
 
 const VisionSection = () => {
   return (
-    <main>
-      
+    <main className='p-[5%] w-full'>
+      {/* main-div */}
+      <div className='flex'>
+        {/* left-div */}
+        <div className='grid grid-cols-6 grid-rows-2 gap-5 w-[50%]'>
+          <div className='col-span-3 row-span-1'>
+            <Image src={v1} />
+          </div>
+          <div className='col-span-3 row-span-1'>
+            <Image src={v2} className='' />
+          </div>
+          <div className='col-span-3 row-span-1'>
+            <Image src={v3} />
+          </div>
+          <div className='col-span-3 row-span-2'>
+            <Image src={v4} />
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
