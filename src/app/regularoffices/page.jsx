c5ba5b4ee8777369802e5de09ff30602547f1ccb
umer12net <umer.net12@gmail.com>
2025-03-22 15:16:50 +0500

    ...

--- src/app/regularoffices/page.jsx
+++ src/app/regularoffices/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const regularoffices = () => {
   return (
     <>
-      <div className="w-full h-[100vh] bg-[url(/regular-office.jpg)] ">
+      <div className="w-full h-[100vh] bg-[url(/p2-banner2.JPG)] bg-cover bg-no-repeat ">
           <div className="filter p-[5%] w-full h-full flex items-center">
             <div>
             <h1 className="main_heading"><span className="yellow">P2</span> Offices</h1>
