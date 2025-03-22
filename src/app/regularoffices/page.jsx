fe42db190efd8244e02733ee0dccfc08d8c94afa
Ahmad <95245638+Ahmaddar007@users.noreply.github.com>
2025-03-22 14:18:31 +0500

    ...

---
+++ src/app/regularoffices/page.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const regularoffices = () => {
+  return (
+    <>
+      <div className="w-full h-[100vh] bg-[url(/regular-office.jpg)] ">
+          <div className="filter p-[5%] w-full h-full flex items-center">
+            <div>
+            <h1 className="main_heading"><span className="yellow">P2</span> Offices</h1>
+            <span className="point text-white">
+              Best for freelancers and Group workings
+            </span>
+            </div>
+          </div>
+      </div>
+    </>
+  );
+};
+
+export default regularoffices;
