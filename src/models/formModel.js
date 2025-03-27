4ad64ac61c2c27c77c5142460b5553aa6ce3e939
Ahmad <95245638+Ahmaddar007@users.noreply.github.com>
2025-03-27 14:13:12 +0500

    model

---
+++ src/models/formModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const formSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+});
+
+const formModel =
+  mongoose.models.formModel || mongoose.model("Form", formSchema);
+
+export default formModel;
+
