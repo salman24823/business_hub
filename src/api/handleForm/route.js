c352a8ee99eb0cb24f8cd959f34eff796285a648
Ahmad <95245638+Ahmaddar007@users.noreply.github.com>
2025-03-28 23:48:37 +0500

    ...

---
+++ src/api/handleForm/route.js
@@ -0,0 +1,11 @@
+import dbConnection from "@/config/dbConnection";
+import formModel from "@/models/formModel";
+import { NextResponse } from "next/server";
+
+export async function POST(req, res) {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
