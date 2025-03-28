2ed0ca8512ce5b90b062bfedcba93fd2532bdf7b
Ahmad <95245638+Ahmaddar007@users.noreply.github.com>
2025-03-29 00:23:12 +0500

    ...

--- src/api/handleForm/route.js
+++ src/api/handleForm/route.js
@@ -3,9 +3,20 @@ import formModel from "@/models/formModel";
 import { NextResponse } from "next/server";
 
 export async function POST(req, res) {
-    try {
-        
-    } catch (error) {
-        
-    }
+  try {
+    await dbConnection();
+    const { name, phone, email, message } = await req.json();
+    const form = new formModel({ name, phone, email, message });
+    await form.save();
+    return NextResponse.json({
+      statusCode: 201,
+      message: "Form submitted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({
+      statusCode: 500,
+      message: "Error saving form data to the database",
+    });
+  }
 }
