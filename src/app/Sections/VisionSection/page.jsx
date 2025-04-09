4a9d9993ec98b0dd3db5178c9a8d2cd959c61ad0
Ahmad <95245638+Ahmaddar007@users.noreply.github.com>
2025-04-09 20:12:03 +0500

    ......

--- src/app/Sections/VisionSection/page.jsx
+++ src/app/Sections/VisionSection/page.jsx
@@ -1,48 +1,9 @@
 import React from "react";
-import Image from "next/image";
-import v1 from "../../../../public/vision1.JPG";
-import v2 from "../../../../public/vision2.jpg";
-import v3 from "../../../../public/vision3.jpg";
-import v4 from "../../../../public/review2.jpg";
 import { Button } from "@heroui/react";
 
 const VisionSection = () => {
   return (
-    <main className="p-[5%] w-full">
-      {/* Main Container */}
-      <div className="flex flex-col lg:flex-row justify-center items-center gap-10">
-        {/* Left-side (Images Grid) */}
-        <div className="grid grid-cols-3 grid-rows-2 gap-3 w-full lg:w-1/2">
-          <div className="col-span-1 row-span-1">
-            <Image
-              src={v1}
-              className="w-full h-auto"
-              alt="Vision 1"
-            />
-          </div>
-          <div className="col-span-2 row-span-1">
-            <Image
-              src={v2}
-              className="w-full h-full"
-              alt="Vision 2"
-            />
-          </div>
-          <div className="col-span-2 row-span-1">
-            <Image
-              src={v3}
-              className="w-full h-full"
-              alt="Vision 3"
-            />
-          </div>
-          <div className="col-span-1 row-span-1">
-            <Image
-              src={v4}
-              className="w-full h-auto"
-              alt="Vision 4"
-            />
-          </div>
-        </div>
-
+    <main className="p-[5%] w-full">    
         {/* Right-side (Text Content) */}
         <div className="w-full lg:w-1/2 text-center lg:text-left space-y-3">
           <span className="point yellow">Our Vision</span>
@@ -63,7 +24,6 @@ const VisionSection = () => {
             Read More
           </Button>
         </div>
-      </div>
     </main>
   );
 };
