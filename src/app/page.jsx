20709e5c6b99b334ba44a83662d099511ad1d36e
Ahmad <95245638+Ahmaddar007@users.noreply.github.com>
2025-04-12 16:39:05 +0500

    ....

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,8 +18,6 @@ export default function Home() {
       <HeroSection />
       <Blank />
       <AboutSection />
-      <About2 />
-
       <MenuSection />
       <FeatureSection />
       <ITservices />
