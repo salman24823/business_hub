7dd98f5c851f3ee18668946b058eb9e5f98b4aa3
Ahmad <95245638+Ahmaddar007@users.noreply.github.com>
2025-04-14 18:22:07 +0500

    offices

--- src/app/premiumoffices/page.jsx
+++ src/app/premiumoffices/page.jsx
@@ -1,66 +1,64 @@
-"use client"; // Add this at the top of the file to make this a client-side component
+"use client";
 
 import { Button } from "@heroui/react";
 import React, { useEffect } from "react";
 import gsap from "gsap";
 import Form from "../Sections/Form/page";
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Image from "next/image";
 
-// Register ScrollTrigger plugin
 gsap.registerPlugin(ScrollTrigger);
 
-const PremiumOffices = () => {
+const RegularOffices = () => {
   const Edata = [
     {
       name: "One-Three Person Offices",
       discription:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam voluptas eos omnis. Unde, perspiciatis aliquam!",
       source: "/v2.JPG",
-      buttonText: "Book Now"
+      buttonText: "Book Now",
     },
     {
       name: "One-Three Person Offices",
       discription:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam voluptas eos omnis. Unde, perspiciatis aliquam!",
       source: "/v2.JPG",
-      buttonText: "Book Now"
+      buttonText: "Book Now",
     },
     {
       name: "One-Three Person Offices",
       discription:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam voluptas eos omnis. Unde, perspiciatis aliquam!",
       source: "/v2.JPG",
-      buttonText: "Book Now"
+      buttonText: "Book Now",
     },
     {
       name: "One-Three Person Offices",
       discription:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam voluptas eos omnis. Unde, perspiciatis aliquam!",
       source: "/v2.JPG",
-      buttonText: "Book Now"
+      buttonText: "Book Now",
     },
     {
       name: "One-Three Person Offices",
       discription:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam voluptas eos omnis. Unde, perspiciatis aliquam!",
       source: "/v2.JPG",
-      buttonText: "Book Now"
+      buttonText: "Book Now",
     },
     {
       name: "One-Three Person Offices",
       discription:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam voluptas eos omnis. Unde, perspiciatis aliquam!",
       source: "/v2.JPG",
-      buttonText: "Book Now"
+      buttonText: "Book Now",
     },
     {
       name: "",
-      discription:
-        "",
+      discription: "",
       source: "",
-      buttonText: ""
-    }
+      buttonText: "",
+    },
   ];
 
   useEffect(() => {
@@ -68,13 +66,11 @@ const PremiumOffices = () => {
     const container = document.getElementById("container");
 
     if (components.length && container) {
-      // Calculate total width of all components
       const totalWidth = Array.from(components).reduce(
         (sum, comp) => sum + comp.offsetWidth,
         0
       );
 
-      // Calculate how much we need to scroll (total width - container width)
       const scrollDistance = totalWidth - container.offsetWidth;
 
       gsap.to(components, {
@@ -85,13 +81,13 @@ const PremiumOffices = () => {
           pin: true,
           scrub: 1,
           end: () => `+=${scrollDistance}`,
-          invalidateOnRefresh: true
-        }
+          invalidateOnRefresh: true,
+        },
       });
     }
 
     return () => {
-      ScrollTrigger.getAll().forEach(instance => instance.kill());
+      ScrollTrigger.getAll().forEach((instance) => instance.kill());
     };
   }, []);
 
@@ -118,54 +114,42 @@ const PremiumOffices = () => {
         </div>
       </div>
 
-      <div
-        id='container'
-        className="w-full bg-white flex flex-col">
-
-        <div 
-        
-        className="sub_heading p-[5%]">
+      <div id="container" className="w-full bg-white flex flex-col">
+        <div className="sub_heading p-[5%]">
           <span className="yellow">Premium</span> Offices
         </div>
 
-        <div className="  w-full">
-
-          <div
-            className="flex gap-4 overflow-x-hidden border-gray-300 border-t px-[5%]"
-          >
-
+        <div className="w-full">
+          <div className="flex gap-4 overflow-x-hidden border-gray-300 border-t px-[5%]">
             {Edata.map((data, index) => (
               <div
                 key={index}
                 className="components flex-shrink-0 flex flex-col items-center bg-white overflow-hidden p-8 border-gray-300 border-r"
-                style={{ width: "calc(33.333% - 16px)" }} // Exactly 1/3 of container width minus gap
+                style={{ width: "calc(33.333% - 16px)" }}
               >
-                <div className="">
-                  <Image
-                    className="w-full h-auto rounded-sm"
-                    src={data.source || "/placeholder.svg"}
-                    alt=""
-                  />
-                </div>
+                {/* 👇 Proper Image Wrapper */}
+                {data.source !== "" && (
+                  <div className="relative w-full h-64">
+                    <Image
+                      src={data.source || "/placeholder.svg"}
+                      alt=""
+                      fill
+                      className="object-cover rounded-sm"
+                    />
+                  </div>
+                )}
 
                 <div className="flex flex-col gap-3 rounded-sm p-3">
                   <strong className="font-bold text-xl">{data.name}</strong>
                   <p className="para text-sm">{data.discription}</p>
-                  {
-                    data.buttonText !== "" ?
-
-                      <Button className="yellow-bg text-white p-3 px-6 font-bold w-full rounded-sm">
-                        {data.buttonText}
-                      </Button>
-                      :
-                      ""
-                  }
+                  {data.buttonText !== "" && (
+                    <Button className="yellow-bg text-white p-3 px-6 font-bold w-full rounded-sm">
+                      {data.buttonText}
+                    </Button>
+                  )}
                 </div>
               </div>
             ))}
-
-            {/* <div className="components w-40 h-auto">ASD</div> */}
-
           </div>
         </div>
       </div>
@@ -175,4 +159,4 @@ const PremiumOffices = () => {
   );
 };
 
-export default PremiumOffices;
+export default RegularOffices;
