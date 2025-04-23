0956e84f6beef3eb2daddb3374f317933a4e581d
Ahmad <95245638+Ahmaddar007@users.noreply.github.com>
2025-04-23 21:31:00 +0500

    .....

--- src/app/api/handleTestimonials/route.js
+++ src/app/api/handleTestimonials/route.js
@@ -6,21 +6,25 @@ export async function POST(req) {
   try {
     await dbConnection();
     const { name, role, message, image } = await req.json();
+
     if (!name || !role || !message || !image) {
       return NextResponse.json(
         { statusCode: 400, message: "All fields are required" },
         { status: 400 }
       );
     }
-    const testimonials = new testimonialModel.save({
+
+    const testimonials = new testimonialModel({
       name,
       role,
       message,
       image,
     });
+
     await testimonials.save();
+
     return NextResponse.json(
-      { statusCode: 201, message: "testimonials added successfully" },
+      { statusCode: 201, message: "Testimonials added successfully" },
       { status: 201 }
     );
   } catch (error) {
