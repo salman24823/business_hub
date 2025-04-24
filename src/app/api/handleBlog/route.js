a04b26fe25e5b35f202f290ef8ecf5aedb6f8c64
Ahmad <95245638+Ahmaddar007@users.noreply.github.com>
2025-04-24 20:01:56 +0500

    Create route.js

---
+++ src/app/api/handleBlog/route.js
@@ -0,0 +1,18 @@
+import dbConnection from "@/config/dbConnection";
+import blogModel from "@/models/blogModel";
+import { NextResponse } from "next/server";
+
+export async function GET(){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+export async function POST(){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
